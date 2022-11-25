7cd3a02098e06da9f755965a1acd13d63b480fc9
Ademsk1 <adam.illoul24@gmail.com>
2022-11-25 18:00:05 +0000

    added try catch

--- static/index.js
+++ static/index.js
@@ -1,12 +1,18 @@
 
 //first test, let the button click activate the robot to move for a short period. 
 async function buttonClick(command) {
-    const res = await fetch(`http://127.0.0.1:5000/${command}`, {
-        method: 'GET',
-        headers: {
-            accept: 'application/json'
-        }
-    })
+    try {
+        const res = await fetch(`http://127.0.0.1:5000/${command}`, {
+            method: 'GET',
+            headers: {
+                accept: 'application/json'
+            }
+        })
+    } catch {
+        console.log(res.status)
+    }
+        
+        print
     data = await res.json()
 }
 function up() {
