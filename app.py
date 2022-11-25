6e5b9de4ae747abf6f2909a31170654a0fcaddd5
Ademsk1 <adam.illoul24@gmail.com>
2022-11-25 15:00:35 +0000

    added static folder with html, css, and js files

--- app.py
+++ app.py
@@ -1,41 +1,45 @@
 import gpiozero
 import time
 from flask import Flask, current_app, jsonify, request
-robot = gpiozero.Robot(left=(17, 18), right=(27, 22))
+# robot = gpiozero.#robot(left=(17, 18), right=(27, 22))
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=['GET'])
 def index():
     return current_app.send_static_file("index.html")
 
 
 @app.route('/right', methods=['GET'])
 def right():
-    robot.right()
+    # robot.right()
     time.sleep(0.25)
-    robot.stop()
+    # robot.stop()
+    return 'ok'
 
 
 @app.route('/left', methods=['GET'])
 def left():
-    robot.left()
+    # robot.left()
     time.sleep(0.25)
-    robot.stop()
+    # robot.stop()
+    return 'ok'
 
 
 @app.route('/forward', methods=['GET'])
 def forward():
-    robot.right()
+    # robot.right()
     time.sleep(0.25)
-    robot.stop()
+    # robot.stop()
+    return 'ok'
 
 
 @app.route('/reverse', methods=['GET'])
 def reverse():
-    robot.reverse()
+    # robot.reverse()
     time.sleep(0.25)
-    robot.stop()
+    # robot.stop()
+    return 'ok'
 
 
 if __name__ == '__main__':
