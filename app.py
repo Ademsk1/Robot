a5c9576e000b5f4af0fd6ed0a463f62f79688388
Ademsk1 <adam.illoul24@gmail.com>
2022-11-25 14:40:03 +0000

    added backend path parameters for going left/right/forward/back and moving the robot respectively

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gpiozero
 import time
 from flask import Flask, current_app, jsonify, request
-
+robot = gpiozero.Robot(left=(17, 18), right=(27, 22))
 app = Flask(__name__)
 
 
@@ -12,4 +12,31 @@ def index():
 
 @app.route('/right', methods=['GET'])
 def right():
-    pass
+    robot.right()
+    time.sleep(0.25)
+    robot.stop()
+
+
+@app.route('/left')
+def left():
+    robot.left()
+    time.sleep(0.25)
+    robot.stop()
+
+
+@app.route('/forward')
+def right():
+    robot.right()
+    time.sleep(0.25)
+    robot.stop()
+
+
+@app.route('/reverse')
+def right():
+    robot.reverse()
+    time.sleep(0.25)
+    robot.stop()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
