bb32e2f6b8c0685db97270e04cac81b412d693a2
Ademsk1 <adam.illoul24@gmail.com>
2022-11-25 15:25:16 +0000

    removing hash so this can be tested on the Pi

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gpiozero
 import time
 from flask import Flask, current_app, jsonify, request
-# robot = gpiozero.#robot(left=(17, 18), right=(27, 22))
+robot = gpiozero.robot(left=(17, 18), right=(27, 22))
 app = Flask(__name__)
 
 
@@ -12,33 +12,33 @@ def index():
 
 @app.route('/right', methods=['GET'])
 def right():
-    # robot.right()
+    robot.right()
     time.sleep(0.25)
-    # robot.stop()
+    robot.stop()
     return 'ok'
 
 
 @app.route('/left', methods=['GET'])
 def left():
-    # robot.left()
+    robot.left()
     time.sleep(0.25)
-    # robot.stop()
+    robot.stop()
     return 'ok'
 
 
 @app.route('/forward', methods=['GET'])
 def forward():
-    # robot.right()
+    robot.right()
     time.sleep(0.25)
-    # robot.stop()
+    robot.stop()
     return 'ok'
 
 
 @app.route('/reverse', methods=['GET'])
 def reverse():
-    # robot.reverse()
+    robot.reverse()
     time.sleep(0.25)
-    # robot.stop()
+    robot.stop()
     return 'ok'
 
 
